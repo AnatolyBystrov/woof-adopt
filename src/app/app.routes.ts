db590247eac03e754786f60bda5f1d2b50848e18
Anatoly <anatolyavbme@gmail.com>
2025-04-24 21:21:13 +0300

    initial project: routing, base layout, structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {SearchDogsComponent} from './pages/search-dogs/search-dogs.component';
+import { AdoptionFormComponent } from './pages/adoption-form/adoption-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'search', pathMatch: 'full'},
+    {path:'search',component:SearchDogsComponent},
+    {path:'adopt',component:AdoptionFormComponent},
+];
---
+++ src/app/pages/search-dogs/search-dogs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-dogs',
+  imports: [],
+  templateUrl: './search-dogs.component.html',
+  styleUrl: './search-dogs.component.css'
+})
+export class SearchDogsComponent {
+
+}
