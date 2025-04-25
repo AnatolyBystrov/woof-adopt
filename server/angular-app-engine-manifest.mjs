2589442267ad04b82ef7018b482b7dd2ea083a80
Anatoly <anatolyavbme@gmail.com>
2025-04-25 21:12:51 +0300

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/woof-adopt',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/woof-adopt/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 716, hash: 'aa547c32ea94b7e830ab035f5baa2c3050ce7f649dc1a26f17be00d9d0f8fa44', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1006, hash: 'ddb49486b04bf37ec94d8562cf8735a5034750f9e1b234168ffa6d53f7a5ba60', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 727, hash: '4a846462023a1f5c305be097695abd2fe21e3378f6abd6832b32741e04cc7637', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1017, hash: 'dfaac9fc64ecf81fd86400ba650f0bd2e0967206d87b6a2132713de1ab71a331', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-PP4BLNOV.css': {size: 598, hash: '5ib/GM1duNQ', text: () => import('./assets-chunks/styles-PP4BLNOV_css.mjs').then(m => m.default)}
   },
 };
